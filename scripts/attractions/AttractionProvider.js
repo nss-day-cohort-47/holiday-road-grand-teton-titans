d4c2ac3b6ca0c5040d6ed1a3a6758f54f3691344
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-15 15:34:42 -0500

    jrg added placeholder html and removed incomplete js modules

--- scripts/attractions/AttractionProvider.js
+++
@@ -1,9 +0,0 @@
-export const getAttractions = () => {
-
-    return fetch("http://holidayroad.nss.team/bizarreries")
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
-}
--- scripts/directions/DirectionProvider.js
+++
@@ -1,9 +0,0 @@
-
-// export const getParks = () => {
-//     return fetch(``)
-//     .then(response => response.json())
-//     .then(parsedResponse => {
-//         // do something with response here
-//         return parsedResponse;
-//     })
-// }
