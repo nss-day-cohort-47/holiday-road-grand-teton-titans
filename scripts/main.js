9386bab93443845d4f0bc837cf272b8d37391c64
ColtMay <colten_mayberry@pm.me>
2021-03-16 10:54:11 -0500

    added function to display eatery to dom

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { attractionList } from "./attractions/AttractionList.js";
 
 import { getEateries } from "./eateries/EateryProvider.js";
 import { eateryList } from "./eateries/EateryList.js";
+import { displayEatery } from "./eateries/EateryPreview.js";
 
 import { getParks } from "./parks/ParkProvider.js";
 import { parkList } from "./parks/ParkList.js";
@@ -10,20 +11,20 @@ import { parkList } from "./parks/ParkList.js";
 const startItinerary = () => {
 
     getAttractions()
-    .then(attractionArray => {
-        attractionList(attractionArray);
-    })
+        .then(attractionArray => {
+            attractionList(attractionArray);
+        })
 
     getEateries()
-    .then(eateryArray => {
-        eateryList(eateryArray);
-    })
+        .then(eateryArray => {
+            eateryList(eateryArray);
+        })
 
     getParks()
-    .then(parksArray => {
-        parkList(parksArray.data);
-    })
-    
+        .then(parksArray => {
+            parkList(parksArray.data);
+        })
+
 }
 
 startItinerary();
