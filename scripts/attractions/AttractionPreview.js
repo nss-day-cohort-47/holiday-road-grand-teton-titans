8029640f6e8e8299e7e4876c4fb4631e2d5232a4
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-16 15:56:37 -0500

    fixed up DOM populator and event listeners! things now display on the DOM

--- scripts/attractions/AttractionPreview.js
+++ scripts/attractions/AttractionPreview.js
@@ -1,9 +1,8 @@
-export const attractionRepresentation = (attractionObj) => {
+export const displayAttraction = (attractionObj) => {
     return `
         <section id="attractionObj" class="attractionObj">
         <p class="attractionObj">${attractionObj.name}</p>
-        <p class="attractionObj"> ${attractionObj.city}</p>
-        <p class="attractionObj"> ${attractionObj.state}</p>
+        <p class="attractionObj"> ${attractionObj.city}, ${attractionObj.state}</p>
         <button id="attractionDetails">Details</button>
 	    </section>
         `
