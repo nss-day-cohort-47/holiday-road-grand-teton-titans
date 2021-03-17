49892214f7f97a5a896407cdd1a95aa7c042622e
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-17 14:48:16 -0500

    console logging the five day forecast 2.

--- scripts/weather/displayWeather.js
+++ scripts/weather/displayWeather.js
@@ -21,3 +21,29 @@ export const weatherLoop = () => {
     }
     DOMLocation.innerHTML = weatherHTML
 }
+
+// "WED"
+// ${parsedResponse.list[0].dt_txt}
+// ${parsedResponse.list[0].main.temp}
+// ${parsedResponse.list[0].weather[0].main}
+// ${parsedResponse.list[0].weather[0].description}
+// "THUR"
+// ${parsedResponse.list[5].dt_txt}
+// ${parsedResponse.list[5].main.temp}
+// ${parsedResponse.list[5].weather[0].main}
+// ${parsedResponse.list[5].weather[0].description}
+// "FRI"
+// ${parsedResponse.list[13].dt_txt}
+// ${parsedResponse.list[13].main.temp}
+// ${parsedResponse.list[13].weather[0].main}
+// ${parsedResponse.list[13].weather[0].description}
+// "SAT"
+// ${parsedResponse.list[21].dt_txt}
+// ${parsedResponse.list[21].main.temp}
+// ${parsedResponse.list[21].weather[0].main}
+// ${parsedResponse.list[21].weather[0].description}
+// "SUN"
+// ${parsedResponse.list[28].dt_txt}
+// ${parsedResponse.list[28].main.temp}}
+// ${parsedResponse.list[28].weather[0].main}
+// ${parsedResponse.list[28].weather[0].description}
