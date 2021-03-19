0babd92a3256c130ccd418d6dbbfd488e5611d64
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-19 09:25:10 -0500

    resaved css stylings to add to main

--- scripts/parks/ParkPreview.js
+++ scripts/parks/ParkPreview.js
@@ -1,14 +1,12 @@
 export const displayPark = (parkObj) => {
     const contentElement = document.querySelector(".parkCard")
-
-
+    let parkDisplay =     `<section id="eaterySection">
                             <h2>${parkObj.fullName}</h2>
                             <p> Location: ${parkObj.addresses[0].city}, ${parkObj.states}</p><br>
                             <p> <a href="${parkObj.url}">Visit the ${parkObj.fullName} official website!</a> </p><br>
                             <button id="parkDetails" value="${parkObj.parkCode}">Detail Snapshot</button>
-
+                            </section>
                         `;
-
     contentElement.innerHTML = parkDisplay;
 }
 
