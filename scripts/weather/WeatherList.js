432c0e7ba2fffdf72bf5c5fc273bff97ebc949e1
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-19 11:54:49 -0500

    styling for the forecast and icon import

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -6,7 +6,7 @@ export const parkWeather = (parkObj) => {
     getWeather(parkCord[0], parkCord[1])
         .then(response => {
             const filteredWeather = weatherFilter(response);
-            // console.log(filteredWeather)
+            console.log(filteredWeather)
             displayWeather(filteredWeather)
         })
 }
@@ -20,7 +20,7 @@ const getLocation = (parkObj) => {
 const weatherFilter = (weatherArray) => {
     let filteredWeatherArray = [];
     weatherArray.forEach(weatherObj => {
-        if (weatherObj.dt_txt.includes('12:00:00')){
+        if (weatherObj.dt_txt.includes('06:00:00')){
             filteredWeatherArray.push(weatherObj);
         }
     });
